c8f3e5a521782da5beb4758eff867bafac778332
Anurag <anuragrajcs2025@gmail.com>
2023-01-14 15:20:56 +0530

    lamest version of the infamous snake game

---
+++ learnings.md
@@ -0,0 +1,53 @@
+# rand() function
+- Under <stdlib.h> function
+- rand() in c generates a psudo-random number. It means that it generates a random number using an or several algorithms(duh!) so that, the number generated are in no way related to each other. But each time you run the program, you will get the same random number of sequence of random numbers.
+
+- ## srand
+    - s: seed
+    - here you can provide a value as a parameter in the function which will act as a seed
+    - what does that mean?
+    ```
+    // suppose we want to create out our random function
+
+    #include <stdio.h>
+    int myRandomNumber(){
+        static int seed = 10;
+        static int random_operation = 0;
+        random_operation = random_operation % 4;
+        switch (random_operation)
+        {
+        case 0:
+            seed += 2;
+            break;
+        case 1:
+            seed /= 4;
+            break;
+        case 2:
+            seed *= 5;
+            break;
+        case 3:
+            seed = seed ^ 4;
+            break;
+        }
+
+        random_operation++;
+        return seed;
+    }
+
+    int main(void){
+        printf("%d\n", myRandomNumber());
+        printf("%d\n", myRandomNumber());
+        printf("%d\n", myRandomNumber());
+        printf("%d\n", myRandomNumber());
+        printf("%d\n", myRandomNumber());
+        printf("%d\n", myRandomNumber());
+        printf("%d\n", myRandomNumber());
+        printf("%d\n", myRandomNumber());
+        printf("%d\n", myRandomNumber());
+
+        return 0;
+    }
+    ```
+    - In the above program, you can notice that the function can kinda generate a random number using that stupid algorithm. This is roughly how to real rand() function works (for the sake of this explanation).
+    - In this program, everytime you call the function you will get a random number, but everytime you run the code you get the same result. This is because we are always starting with 10 (seed). But if we everytime we give a different seed value, we will get a different result.
+    - This is kinda how we use **srand** function. The most used seed value is **time()**.     
---
+++ src.c
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <unistd.h> // for sleep and getch
+
+#define height 25
+#define width 50
+
+#define clearScreen() printf("\033[H\033[J");
+
+int getch(void)
+{
+	struct termios oldt, newt;
+	int ch;
+
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	ch = getchar();
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+	return ch;
+}
+
+typedef struct{
+    int x_position;
+    int y_position;
+    char shape;
+} Snake;
+
+typedef struct{
+    int x_position;
+    int y_position;
+} Fruit;
+
+int game_over = 0;
+int heighest_score = 0;
+
+void gameOver(int* score, Snake* snake){
+        clearScreen();
+
+        printf("\t\t\t\tG\tA\tM\tE\t\tO\tV\tE\tR\t!\n");
+        printf("\tYour Score: %d\n", *score);
+        if(heighest_score < *score) heighest_score = *score;
+        printf("\tHeighest Score: %d\n", heighest_score);
+
+
+        printf("\t\tpress any key to continue or q to quit: ");
+        char key = getch();
+        if(key == 'q' || key == 'Q') exit(0);
+        *score = 0;
+}
+
+Fruit putFruit(){
+    Fruit fruit;
+    fruit.x_position = rand()%(width-1);
+    fruit.y_position = rand()%(height-1);
+
+    // we might get either of them 0, that would place fruit to the border
+    while(fruit.x_position == 0) fruit.x_position = rand()%width;
+    while(fruit.y_position == 0) fruit.y_position = rand()%height;
+
+    return fruit;
+}
+
+void snakeShape(int shapes){
+    switch(shapes){
+        case 1:
+            printf("~:");
+            break;
+        case 2:
+            printf(":~");
+            break;
+        case 3:
+            printf("!");
+            break;
+        case 4:
+            printf("i");
+            break;
+    }
+}
+
+Fruit drawArea(Fruit fruit, Snake snake){
+    clearScreen();
+    static int score = 0;
+
+    for(int i = 0; i < height; i++){
+        if(i != 0)
+            printf("|");
+        else printf(" ");
+
+        for(int j = 0; j < width; j++){
+            if(i == 0 || i == height-1){
+                printf("_");
+                continue;
+            }
+
+            if(i == fruit.y_position && j == fruit.x_position){
+                if(i == snake.y_position && j == snake.x_position){
+                    snakeShape(snake.shape);
+                    fruit = putFruit();
+                    score += 10;
+                }
+                else printf("*");
+                continue;  
+            }
+
+            if(i == snake.y_position && j == snake.x_position){
+                snakeShape(snake.shape);
+                continue;
+            }
+
+            printf(" ");
+        }
+
+        if(i != 0)
+            printf("|\n");
+        else printf("\n");
+    }
+    if(snake.y_position <= 0 || snake.y_position >= height-1 || snake.x_position <= 0 || snake.x_position >= width-1){
+        game_over = 1;
+        gameOver(&score, &snake);
+        main();
+    }
+
+    printf("\t\t Score: %d\n", score);
+    if(heighest_score < score) heighest_score = score;
+    printf("\t\t Heighest Score: %d\n", heighest_score);
+
+    return fruit;
+}
+ 
+Snake input(Snake snake){
+    static int snake_x_position = 1;
+    static int snake_y_position = 1;
+    
+    if(game_over == 1){
+        snake_x_position = 1;
+        snake_y_position = 1;
+        game_over = 0;
+    }
+
+    char user_direction;
+    // scanf("%c", &user_direction); // getch, getchar, getc not working
+    user_direction = getch();
+    switch (user_direction)
+    {
+        case 'w':
+            snake_y_position--;
+            snake.shape = 4;
+            break;
+        case 's':
+            snake_y_position++;
+            snake.shape = 3;
+            break;
+        case 'd':
+            snake_x_position++;
+            snake.shape = 1;
+            break;
+        case 'a':
+            snake_x_position--;
+            snake.shape = 2;
+            break;
+    }
+
+    snake.y_position = snake_y_position;
+    snake.x_position = snake_x_position;
+
+
+    return snake;
+}
+
+int main(void){
+
+    Fruit fruit;
+    Snake snake;
+    snake.x_position = 1;
+    snake.y_position = 1;
+    snake.shape = 1;    // snake.shape(1: '~:', 2: ':~', 3: '!', 4: 'i')
+
+    fruit = putFruit();
+
+    while(1)
+    {
+        fruit = drawArea(fruit, snake);
+        snake = input(snake);
+        sleep(0.2);
+    }
+
+    return 0;
+}
